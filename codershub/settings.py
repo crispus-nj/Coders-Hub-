ed0736fd359660925fb685cdb42fc75ea7484304
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-02-26 10:31:32 +0300

    adjusted the setting.py configuration in setting section

--- codershub/settings.py
+++ codershub/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
-# SECRET_KEY = 'django-insecure-r-o3)zd38binl(7+r!z5!2gbgst-69oc2h_d3&x^kr%+gmtfpw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
